608287ca747ecf3ea745ad813bc4b08142ca9478
Erik Hebisch <flegelleicht@gmail.com>
2019-02-01 15:44:35 +0100

    Changed saving uploaded files to thumbor instead of file system

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -4,12 +4,29 @@ class UploadController < ApplicationController
 
 	def upload
 		picture = upload_params[:picture]
-		File.open(Rails.root.join('public', 'uploads', picture.original_filename), 'wb') do |file|
-			file.write(picture.read)
+		location = upload_to_thumbor(picture)
+		Image.create do |image|
+			image.name = picture.original_filename
+			image.path = location
 		end
+		redirect_to uploads_path
 	end
+
+	private
 
 	def upload_params
 		params.permit(:picture)
 	end
+
+	def upload_to_thumbor(file)
+		require "net/http"
+		uri = URI("http://thumbor:8888/image")
+		http = Net::HTTP.new(uri.host, uri.port)
+		req = Net::HTTP::Post.new(uri.path)
+		req.body = IO.read(file.tempfile)
+		req.content_type = "image/jpeg"
+		res = http.request(req)
+		res.each_header.to_h["location"]
+	end
 end
+
